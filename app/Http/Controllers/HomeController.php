9ada5e59dbdea4d76e9b87077fb58d31509900fa
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-09-19 23:15:32 +0600

    ssl commerze integration with sandbox

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index(): string
     {
-        return dashboard();
+        return view('exampleEasycheckout');
     }
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/pay-via-ajax',
+        '/success',
+        '/cancel',
+        '/fail',
+        '/ipn'
     ];
 }
