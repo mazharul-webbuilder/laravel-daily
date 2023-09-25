d1235e48f502f56ee39b168ed5d804284d6f212d
irozen-mazharul <141237975+irozen-mazharul@users.noreply.github.com>
2023-09-25 14:41:54 +0600

    collection concept

--- routes/laravelCollection.php
+++ routes/laravelCollection.php
@@ -3,7 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaravelCollectionController;
 
+
+/*Every data retrieve from database are in collection form
+You can perform any action laravel support over the retrieve data
+
+Collection is a more extend version of PHP array offer by Laravel
+*/
 Route::prefix('laravel-collection')->controller(LaravelCollectionController::class)->group(function (){
-    Route::get('/', 'index');
+    Route::get('/basic-filter', 'index');
+    Route::get('/macro', 'macro');
+    Route::get('/chunk', 'chunk');
+    Route::get('/excepts', 'excepts');
 });
 
