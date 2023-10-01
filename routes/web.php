89c8edef0d3d0bb90f556deb3bac6adb83209d20
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-01 21:57:25 +0600

    set role permission enviornment

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 require __DIR__ . '/eloquent-relationship.php';
 require __DIR__ . '/mail.php';
 require __DIR__ . '/multi-auth.php';
+require __DIR__ . '/role_permission.php';
 
 /*Twilio SMS OTP Sent*/
 Route::get('sendSmsOTP', [TwilioSMSController::class, 'index']);
